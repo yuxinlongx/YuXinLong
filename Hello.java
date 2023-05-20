8458656eaea94da51c931371242e4bb1f623cc76
Yu XinLong <2509780966@qq.com>
2023-05-20 22:58:01 +0800

    add Hello

---
+++ Hello.java
@@ -0,0 +1,15 @@
+//1.public class Hello{}表示Hello是一个piblic共有类
+//2.Hello{}表示一个类的开始结束
+//3.public static voild main(String[] aegs)表示一个主方法，即我们程序的入口
+//4.main(){} 表示方法的开始结束
+//5.System.out.println("hello,world~")输出到屏幕
+//6.;语句结束
+public class Hello{
+
+    //编写一个mian方法
+	public static void main(String[] args){
+	  System.out.println("hello,world~");
+	}
+
+
+}
